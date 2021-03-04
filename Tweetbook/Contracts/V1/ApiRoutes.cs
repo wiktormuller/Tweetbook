e1f742cbaf69dcf8a1cb55ffa5d0dd92b96dd261
Wiktor Muller <biznes.wiktor@gmail.com>
2021-03-04 15:44:38 +0100

    Add versioning

---
+++ Tweetbook/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,14 @@
+﻿namespace Tweetbook.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        private const string Root = "api";
+        private const string Version = "v1";
+        private const string Base = Root + "/" + Version;
+        
+        public static class Posts
+        {
+            public const string GetAll = Base + "/posts";
+        }
+    }
+}
---
+++ Tweetbook/Controllers/V1/PostsController/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Tweetbook.Contracts.V1;
+using Tweetbook.Domain;
+
+namespace Tweetbook.Controllers.V1.PostsController
+{
+    public class PostsController : Controller
+    {
+        private readonly List<Post> _posts;
+
+        public PostsController()
+        {
+            _posts = new List<Post>();
+            for (int i = 0; i < 5; i++)
+            {
+                _posts.Add(new Post
+                {
+                    Id = Guid.NewGuid().ToString()
+                });
+            }
+        }
+        
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+    }
+}
