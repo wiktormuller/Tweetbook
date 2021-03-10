a99e4eb06a0acc9e87d9fc2bc2fc117e24861074
Wiktor Muller <biznes.wiktor@gmail.com>
2021-03-10 16:02:23 +0100

    Setting up ApiKey-based Authentication

---
+++ Tweetbook/Controllers/V1/SecretController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tweetbook.Filters;
+
+namespace Tweetbook.Controllers.V1.PostsController
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("seret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I have no secrets");
+        }
+    }
+}
